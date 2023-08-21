65da5b12228b3d8c97fb30a49192efb044c0b4f4
Fabian Vieri <42199171+fabianvieri@users.noreply.github.com>
2023-08-21 10:43:31 +0000

    use forwardRef in custom input

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,26 +1,23 @@
 import styled from "@emotion/styled";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, forwardRef } from "react";
 
 type FormInputProps = HTMLAttributes<HTMLInputElement> & {
   isError: boolean;
 };
 
 const Input = styled.input<FormInputProps>`
+  outline: none;
+  border: none;
+  padding: 10px 10px 10px 0px;
+  font-size: 1rem;
+  border-bottom: 1px solid ${(props) => (props.isError ? "red" : "grey")};
+  &:focus {
     outline: none;
-	border: none;
-	padding: 10px 10px 10px 0px;
-	font-size: 1rem;
-    border-style:solid;
-    border-width:5px;
-    border-color:${(props) => (props.isError ? "hotpink" : "grep")}
-
-    &:focus {
-		outline: none;
-	}
+  }
 `;
 
-function FormInput(props: FormInputProps) {
-  return <Input {...props} />;
-}
+const FormInput = forwardRef<HTMLInputElement, FormInputProps>((props, ref) => (
+  <Input {...props} ref={ref} />
+));
 
 export default FormInput;
